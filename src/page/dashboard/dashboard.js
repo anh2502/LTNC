bcefa8c3799f0008ddce5482b8cc8905f72487ee
AnhAnh1212 <kavilnina13@gmail.com>
2024-04-09 15:28:28 +0700

    Themlich

--- src/page/dashboard/dashboard.js
+++ src/page/dashboard/dashboard.js
@@ -1,13 +1,15 @@
-import { Typography } from '@mui/material';
 import '../../App.css'
 import React, { useState } from "react";
-
-function HomePage() {
-  return (
-    <div className="App" style={{ width: '100%', textAlign: 'center', paddingTop: '20%' }}>
-      <Typography variant='h1' fontWeight={900} fontFamily={'Lato'} color={'#303972'}>HELLO WORLD!</Typography>
-    </div >
-  );
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+export default class DemoApp extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+      
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
+    )
+  }
 }
-
-export default HomePage;
